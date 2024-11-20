41e8f1fd89ee7faddbb492461c02da3fe2e8e2f9
seeeeeeong <lsinsung@gmail.com>
2024-11-20 21:21:46 +0900

    init project

---
+++ src/main/java/lee/io/lingualog/LingualogApplication.java
@@ -0,0 +1,13 @@
+package lee.io.lingualog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingualogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingualogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lee/io/lingualog/LingualogApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.io.lingualog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingualogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
